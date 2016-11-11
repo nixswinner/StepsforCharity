53eb3c820b0fb497c909e33aed63a186c2fb4d49
valentinerutto <rutto28@gmail.com>
2016-11-12 02:46:40 +0300

    changed buildtoolversion

--- app/src/main/java/com/example/valentine/stepsforcharity/SignupActivity.java
+++ app/src/main/java/com/example/valentine/stepsforcharity/SignupActivity.java
@@ -6,9 +6,23 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import butterknife.BindView;
 
 public class SignupActivity extends AppCompatActivity {
+    private static final String TAG = "SignupActivity";
 
+    @BindView(R.id.input_name) EditText _nameText;
+    @BindView(R.id.input_email) EditText _emailText;
+    @BindView(R.id.input_password)
+    EditText _passwordText;
+    @BindView(R.id.btn_signup)
+    Button _signupButton;
+    @BindView(R.id.link_login)
+    TextView _loginLink;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
